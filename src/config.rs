ec7bfa504bd94970cfd126268d78dba7c551851d
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2026-02-09 17:31:30 +0000

    Copy default config on first run

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,7 @@ use serde::Deserialize;
 use crate::error::DaemonError;
 
 const CONFIG_PATH: &str = ".config/bar_daemon/config.toml";
+const DEFAULT_CONFIG_PATH: &str = "/etc/bar_daemon/config.toml";
 
 /// # Documentation
 /// The `Config` derived from the `config.toml` file
@@ -31,26 +32,35 @@ impl Default for Config {
 // TODO This just panics right now since without CONFIG this daemon can't function
 static CONFIG: LazyLock<Config> = LazyLock::new(|| {
     // Get the $HOME directory for this user
-    let home_os_str = std::env::var_os("HOME")
-        .map_or_else(|| Err(DaemonError::PathCreateError(String::from("env $HOME"))), Ok)
-        .unwrap_or_else(|e| panic!("{e}"));
+    let home_os_str =
+        std::env::var_os("HOME").unwrap_or_else(|| panic!("{}", DaemonError::PathCreateError(String::from("env $HOME"))));
 
     // Convert $HOME to &str
-    let home = home_os_str
-        .to_str()
-        .map_or_else(
-            || {
-                Err(DaemonError::PathCreateError(String::from(
-                    "env $HOME could not convert to &str",
-                )))
-            },
-            Ok,
+    let home = home_os_str.to_str().unwrap_or_else(|| {
+        panic!(
+            "{}",
+            DaemonError::PathCreateError(String::from("env $HOME could not convert to &str",))
         )
-        .unwrap_or_else(|e| panic!("{e}"));
+    });
 
     // Combine $HOME and CONFIG_PATH to create a full path to the config file
     let config_path_str = format!("{home}/{CONFIG_PATH}");
     let config_path = Path::new(config_path_str.as_str());
+
+    // If the config_path doesn't point to any file, copy it from /etc/
+    if !config_path.exists() {
+        // Create the config_path parent folders
+        fs::create_dir_all(config_path.parent().unwrap_or_else(|| {
+            panic!(
+                "{}",
+                DaemonError::PathCreateError(String::from("Could get parent of `config_path`"))
+            )
+        }))
+        .unwrap_or_else(|e| panic!("{}", DaemonError::PathCreateError(e.to_string())));
+
+        // Copy the default config from /etc/
+        fs::copy(DEFAULT_CONFIG_PATH, &config_path).unwrap_or_else(|e| panic!("{}", DaemonError::PathRwError(e.to_string())));
+    }
 
     // Read the config file as a String (Converting Error to DaemonError::PathRwError)
     let config = fs::read_to_string(config_path)
