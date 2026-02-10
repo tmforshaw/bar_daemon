3c754e0ae5c009c2f12b07bb8b6cda3db21d524e
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2026-02-10 22:04:13 +0000

    Remove newline for errors since it clogs the logs

--- src/error.rs
+++ src/error.rs
@@ -5,37 +5,37 @@ use crate::listener::ClientMessage;
 
 #[derive(Error, Debug)]
 pub enum DaemonError {
-    #[error("Socket Error:\n\t\"{0}\"")]
+    #[error("Socket Error:\t\"{0}\"")]
     SocketError(#[from] tokio::io::Error),
 
-    #[error("Postcard Serialize/Deserialize Error:\n\t\"{0}\"")]
+    #[error("Postcard Serialize/Deserialize Error:\t\"{0}\"")]
     PostcardError(#[from] postcard::Error),
 
-    #[error("Command '{name}' With Args '{args:?}' Could Not Run:\n\t\"{e}\"")]
+    #[error("Command '{name}' With Args '{args:?}' Could Not Run:\t\"{e}\"")]
     CommandError { name: String, args: Vec<String>, e: String },
 
-    #[error("Bytes Could Not Convert To String:\n\t\"{0}\"")]
+    #[error("Bytes Could Not Convert To String:\t\"{0}\"")]
     IntegerFromByteString(#[from] std::string::FromUtf8Error),
 
-    #[error("String Could Not Convert To Integer:\n\t\"{0}\"")]
+    #[error("String Could Not Convert To Integer:\t\"{0}\"")]
     IntegerFromString(#[from] std::num::ParseIntError),
 
-    #[error("String Could Not Convert To Bool:\n\t\"{0}\"")]
+    #[error("String Could Not Convert To Bool:\t\"{0}\"")]
     BoolFromString(#[from] std::str::ParseBoolError),
 
-    #[error("String Could Not Convert To Float:\n\t\"{0}\"")]
+    #[error("String Could Not Convert To Float:\t\"{0}\"")]
     StringToFloatError(#[from] std::num::ParseFloatError),
 
-    #[error("String could not parse enough arguments:\n\t\"{0}\"")]
+    #[error("String could not parse enough arguments:\t\"{0}\"")]
     ParseError(String),
 
-    #[error("Serde JSON Serialization Failed:\n\t\"{0}\"")]
+    #[error("Serde JSON Serialization Failed:\t\"{0}\"")]
     JsonError(#[from] serde_json::Error),
 
-    #[error("Mpsc Could Not Send ClientMessage:\n\t\"{0}\"")]
+    #[error("Mpsc Could Not Send ClientMessage:\t\"{0}\"")]
     MpscSendError(#[from] mpsc::error::SendError<ClientMessage>),
 
-    #[error("Could not convert between int types:\n\t\"{0}\"")]
+    #[error("Could not convert between int types:\t\"{0}\"")]
     IntError(#[from] std::num::TryFromIntError),
 
     #[error("RwLock couldn't be locked")]
@@ -44,10 +44,10 @@ pub enum DaemonError {
     #[error("Could not convert usize to TupleName")]
     TupleNameError,
 
-    #[error("Could not create path:\n\t\"{0}\"")]
+    #[error("Could not create path:\t\"{0}\"")]
     PathCreateError(String),
 
-    #[error("Could not read/write to path:\n\t\"{0}\"")]
+    #[error("Could not read/write to path:\t\"{0}\"")]
     PathRwError(String),
 
     #[error("Monitored value of type '{0}' in Snapshot was None")]
