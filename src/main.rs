2b286c43c9aabbf68b1a4dc658bf73997fd1ee23
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2026-02-17 15:16:34 +0000

    Adding TODOs

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use crate::{cli::evaluate_cli, error::DaemonError, logging::init_logging};
 // TODO notify listeners when the read_until_valid function finds a new valid value
 // TODO Call read_until_valid again after a long wait if the maximum number of attempts have been reached.
 // TODO Socket handler shutdown successfully shows up twice in the INFO
+// TODO Change cached Volume percent by the agreed amount, and display that value so that the log conversions don't mess it up
 
 pub mod battery;
 pub mod bluetooth;
